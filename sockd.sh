742c88eeff8414524d7cc35963478de5c527626b
0dataexpert0 <56006577+0dataexpert0@users.noreply.github.com>
2019-12-03 00:13:07 +0000

    Update sockd.sh

--- sockd.sh
+++ sockd.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -e
 [ -f /etc/openvpn/up.sh ] && /etc/openvpn/up.sh "$@"
-pidof sockd | xargs --no-run-if-empty kill -TERM
-exec /usr/sbin/sockd -f /etc/sockd.conf
+/usr/sbin/sockd -D
